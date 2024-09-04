c76ea5816c436a7029db0a45b3eb5410dda6b542
Yago <ylm@icomp.ufam.edu.br>
2024-09-04 19:32:52 -0400

    feat: Add Button in task list and chore: Add and setup from lucide-icon for react

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,17 @@
+import { ChevronRightIcon } from "lucide-react";
+
 function Tasks({ tasks }) {
   return (
-    <ul className="space-y-4">
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
-        <li className="bg-slate-400 text-white p-2 rounded-md">{task.title}</li>
+        <li key={task.id} className="flex gap-2">
+          <button className="bg-slate-400 text-white p-2 rounded-md w-full text-left">
+            {task.title}
+          </button>
+          <button className="bg-slate-400 text-white p-2 rounded-md">
+            <ChevronRightIcon />
+          </button>
+        </li>
       ))}
     </ul>
   );
