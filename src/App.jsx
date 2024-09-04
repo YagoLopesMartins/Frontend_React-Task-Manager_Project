fdeaddd86cd121acc0287552d94e220cf294c43e
Yago <ylm@icomp.ufam.edu.br>
2024-09-04 19:48:05 -0400

    feat: Add behavior on button when click and conditional in css to render line-through in list item

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,27 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
         <AddTasks />
       </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,16 @@
 import { ChevronRightIcon } from "lucide-react";
 
-function Tasks({ tasks }) {
+function Tasks({ tasks, onTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-white p-2 rounded-md w-full text-left">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`${
+              task.isCompleted && "line-through"
+            } bg-slate-400 text-white p-2 rounded-md w-full text-left`}
+          >
             {task.title}
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
