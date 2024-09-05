bd28cd2d2c89974c760c9e9cb219c61b4f376162
Yago <ylm@icomp.ufam.edu.br>
2024-09-04 21:14:29 -0400

    feat: Add behavior on button delete click

--- src/App.jsx
+++ src/App.jsx
@@ -39,13 +39,22 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
         <AddTasks />
       </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,6 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, Trash2 } from "lucide-react";
 
-function Tasks({ tasks, onTaskClick }) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -15,6 +15,12 @@ function Tasks({ tasks, onTaskClick }) {
           </button>
           <button className="bg-slate-400 text-white p-2 rounded-md">
             <ChevronRightIcon />
+          </button>
+          <button
+            className="bg-slate-400 text-white p-2 rounded-md"
+            onClick={() => onDeleteTaskClick(task.id)}
+          >
+            <Trash2 />
           </button>
         </li>
       ))}
