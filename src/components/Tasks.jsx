3597a0373db456e876c91719820018ce628bde4b
Yago <ylm@icomp.ufam.edu.br>
2024-09-04 22:34:19 -0400

    feat: Add logical from router about task details with query params

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 import { ChevronRightIcon, Trash2 } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?title=${query.toString()}`);
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
@@ -13,7 +23,10 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
           >
             {task.title}
           </button>
-          <button className="bg-slate-400 text-white p-2 rounded-md">
+          <button
+            className="bg-slate-400 text-white p-2 rounded-md"
+            onClick={() => onSeeDetailsClick(task)}
+          >
             <ChevronRightIcon />
           </button>
           <button
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,5 +1,15 @@
+import { useSearchParams } from "react-router-dom";
+
 function TaskPage() {
-  return <h1>Details</h1>;
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
 }
 
 export default TaskPage;
